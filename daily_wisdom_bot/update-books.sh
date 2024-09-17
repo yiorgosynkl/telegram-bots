ada1e660aa7b6dfcf34eeec572a35be510590c56
Georgios <giorgosgnkl@gmail.com>
2024-09-17 23:33:09 +0100

    update git clone path

--- daily_wisdom_bot/update-books.sh
+++ daily_wisdom_bot/update-books.sh
@@ -1,4 +1,4 @@
 # update books with this script
 rm -rf books-txt-collection/
-git clone https://github.com/yiorgosynkl/bot-txt-collection.git books-txt-collection
+git clone https://github.com/yiorgosynkl/books-txt-collection.git books-txt-collection
 rm -rf books-txt-collection/.git/
